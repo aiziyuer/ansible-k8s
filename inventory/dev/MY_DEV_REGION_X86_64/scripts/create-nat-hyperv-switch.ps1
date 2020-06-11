2508242f9f6a186f584b47c5b0f001993f20474f
aiziyuer <ziyu0123456789@gmail.com>
2020-06-11 11:47:45 -0400

    improve vagrant

--- inventory/dev/MY_DEV_REGION_X86_64/scripts/create-nat-hyperv-switch.ps1
+++ inventory/dev/MY_DEV_REGION_X86_64/scripts/create-nat-hyperv-switch.ps1
@@ -5,28 +5,28 @@ If ("NAT-Switch" -in (Get-VMSwitch | Select-Object -ExpandProperty Name) -eq $FA
 
     New-VMSwitch -SwitchName "NAT-Switch" -SwitchType Internal
 
-    New-NetIPAddress -IPAddress 192.168.0.1 -PrefixLength 24 -InterfaceAlias "vEthernet (NAT-Switch)"
+    New-NetIPAddress -IPAddress 192.168.200.1 -PrefixLength 24 -InterfaceAlias "vEthernet (NAT-Switch)"
 
-    New-NetNAT -Name "NATNetwork" -InternalIPInterfaceAddressPrefix 192.168.0.0/24
+    New-NetNAT -Name "NATNetwork" -InternalIPInterfaceAddressPrefix 192.168.200.0/24
 }
 else {
     '"NAT-Switch" for static IP configuration already exists; skipping'
 }
 
-If ("192.168.0.1" -in (Get-NetIPAddress | Select-Object -ExpandProperty IPAddress) -eq $FALSE) {
-    'Registering new IP address 192.168.0.1 on Windows Hyper-V host...'
+If ("192.168.200.1" -in (Get-NetIPAddress | Select-Object -ExpandProperty IPAddress) -eq $FALSE) {
+    'Registering new IP address 192.168.200.1 on Windows Hyper-V host...'
 
-    New-NetIPAddress -IPAddress 192.168.0.1 -PrefixLength 24 -InterfaceAlias "vEthernet (NAT-Switch)"
+    New-NetIPAddress -IPAddress 192.168.200.1 -PrefixLength 24 -InterfaceAlias "vEthernet (NAT-Switch)"
 }
 else {
-    '"192.168.0.1" for static IP configuration already registered; skipping'
+    '"192.168.200.1" for static IP configuration already registered; skipping'
 }
 
-If ("192.168.0.0/24" -in (Get-NetNAT | Select-Object -ExpandProperty InternalIPInterfaceAddressPrefix) -eq $FALSE) {
-    'Registering new NAT adapter for 192.168.0.0/24 on Windows Hyper-V host...'
+If ("192.168.200.0/24" -in (Get-NetNAT | Select-Object -ExpandProperty InternalIPInterfaceAddressPrefix) -eq $FALSE) {
+    'Registering new NAT adapter for 192.168.200.0/24 on Windows Hyper-V host...'
 
-    New-NetNAT -Name "NATNetwork" -InternalIPInterfaceAddressPrefix 192.168.0.0/24
+    New-NetNAT -Name "NATNetwork" -InternalIPInterfaceAddressPrefix 192.168.200.0/24
 }
 else {
-    '"192.168.0.0/24" for static IP configuration already registered; skipping'
+    '"192.168.200.0/24" for static IP configuration already registered; skipping'
 }
